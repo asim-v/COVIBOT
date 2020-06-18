29c65217681f887d61aa4708467620efe536170a
Asim-v <emoxic.sama@gmail.com>
2020-06-18 17:40:55 -0500

    pull del query ya sirvmdts

--- UI/Flask/app.py
+++ UI/Flask/app.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template
 from flask_session import Session
-import pyrebase
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-config = {
-  "apiKey": "AIzaSyDVpxOSaZH08jzKvQJhssW06sloEHz5voc",
-  "authDomain": "colabotz.firebaseapp.com",
-  "databaseURL": "https://colabotz.firebaseio.com",
-  "storageBucket": "colabotz.appspot.com",
-  "serviceAccount": "keyfile.json"
-}
-firebase = pyrebase.initialize_app(config)
 
-db = firebase.database()
-db.child("colabotz").child("extraction")
+# cred = credentials.Certificate('keyfile.json')
+# firebase_admin.initialize_app(cred,{"databaseURL":"https://colabotz.firebaseio.com"})
+
+
+# Import database module.
+from firebase_admin import db
+
+ref = db.reference('extraction')
+#snapshot = ref.order_by_key().limit_to_first(1).get()
+snapshot = ref.order_by_child('rt_OgRetwCount').limit_to_last(2).get()
+
+
+
+print(json.dumps(snapshot, indent=4, sort_keys=True))
+
+#posts = db.child("extraction").shallow().limit_to_first(5).get()  #Funciona
+#posts = db.child("extraction").order_by_key().get()               #Funciona
+#posts = db.child("extraction").order_by_child("rt_OgFavCount").limit_to_first(5).get()#.val()
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template("index.html",error = 0)
+	return render_template("index.html")	
